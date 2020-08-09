60c71fc42ae0f148857a173321ee16400ec8e7d4
Steven <steven.knoblich@web.de>
2020-08-09 19:53:23 +0200

    Add base information for API/HTTP client

---
+++ api/baseInformation.go
@@ -0,0 +1,8 @@
+package api
+
+const baseUrl = "https://my.reflex.rip/api/v1"
+const loginUrl = baseUrl + "/auth"
+const whitelistPatchUrl = baseUrl + "/whitelist/ips"
+const ipUrl = "https://ipv4.reflex.rip"
+
+const userAgent = "IPUpdate/1.0"
